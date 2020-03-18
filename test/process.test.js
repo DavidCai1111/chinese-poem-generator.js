5e4d47b23e68fd677137e92932de4e5f58df62b4
David Cai <davidcai1993@yahoo.com>
2020-03-18 18:20:39 +0800

    feat: get inputSequences

--- test/process.test.js
+++ test/process.test.js
@@ -6,11 +6,18 @@ const assert = require('power-assert')
 const process = require('../lib/process')
 
 describe('process', function () {
+  const json = fs.readFileSync(path.join(__dirname, './data/poet.song.91000.json'), 'utf8')
+
   it('readJSON', async function () {
-    const json = fs.readFileSync(path.join(__dirname, './data/poet.song.91000.json'), 'utf8')
     const result = await process.readJSON(json)
 
-    assert.strictEqual(typeof result, 'string')
-    assert.strictEqual(result.startsWith('扣橈泛澄虚'), true)
+    assert.strictEqual(typeof result.allStr, 'string')
+    assert.strictEqual(result.allStr.startsWith('扣橈泛澄虚'), true)
+    assert.strictEqual(result.charSet.has('扣'), true)
+    assert.strictEqual(result.charSet.has('，'), true)
+    assert.strictEqual(result.charSet.has('。'), true)
+    assert.strictEqual(result.charSet.has('扣橈'), false)
+    assert.strictEqual(result.charSet.size > 100, true)
+    assert.strictEqual(result.maxParagraphLen, 27)
   })
 })
