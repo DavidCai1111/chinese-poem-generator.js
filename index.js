05a677e3c1e72e75d2c5ffe80bcb107d3883862f
David Cai <davidcai1993@yahoo.com>
2020-03-18 20:39:26 +0800

    feat: add model

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 'use strict'
+require('@tensorflow/tfjs-node')
+const tf = require('@tensorflow/tfjs')
+const path = require('path')
+const model = require('./lib/model')
+const process = require('./lib/process')
+
+;(async function () {
+  const preprocessResult = await process.readJSONFile(path.join(__dirname, './test/data/poet.song.91000.json'))
+
+  const rnnModel = model.getGeneratorModel(preprocessResult)
+
+  const dataSet = tf.data.generator(function () {
+    let i = 0
+    const batchSize = 3000
+
+    const iterator = {
+      next: () => {
+        const value = {
+          xs: tf.tensor(preprocessResult.inputs.slice(i, i + batchSize)),
+          ys: tf.tensor(preprocessResult.labelsOnehot.slice(i, i + batchSize))
+        }
+
+        i = i + batchSize
+
+        const done = i >= preprocessResult.labelsOnehot.length
+
+        return { value, done }
+      }
+    }
+
+    return iterator
+  })
+
+  await rnnModel.fitDataset(
+    dataSet,
+    { epochs: 100, verbose: 1 }
+  )
+
+  await rnnModel.save(`file://${path.join(__dirname, './assets')}`)
+})().catch(console.error)
+
--- test/process.test.js
+++ test/process.test.js
@@ -1,9 +1,11 @@
 /* global describe it */
 'use strict'
+require('@tensorflow/tfjs-node')
 const fs = require('fs')
 const path = require('path')
 const assert = require('power-assert')
 const process = require('../lib/process')
+const model = require('../lib/model')
 
 describe('process', function () {
   const json = fs.readFileSync(path.join(__dirname, './data/poet.song.91000.json'), 'utf8')
@@ -19,7 +21,12 @@ describe('process', function () {
     assert.strictEqual(result.charSet.has('扣橈'), false)
     assert.strictEqual(result.charSet.size > 100, true)
     assert.strictEqual(result.maxParagraphLen, 27)
+    assert.strictEqual(result.inputs.length, result.inputSequences.length)
+    assert.strictEqual(result.inputs[10].length, result.inputSequences[10].length - 1)
+    assert.strictEqual(result.labels.length, result.inputs.length)
+    assert.strictEqual(result.labelsOnehot[50][48], 1)
+    assert.strictEqual(result.labelsOnehot[50].length, result.charSetArray.length)
 
-    console.log(result.inputSequences)
+    model.getGeneratorModel(result)
   })
 })
